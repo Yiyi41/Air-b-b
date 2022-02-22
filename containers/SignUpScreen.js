a5b75c74305d0d3b8e5fadf3382a3938c84a2e5f
Yiyi <loveloveyy@hotmail.com>
2022-02-22 15:41:51 +0100

    first commit

--- containers/SignUpScreen.js
+++ containers/SignUpScreen.js
@@ -1,20 +1,90 @@
-import { Button, Text, TextInput, View } from "react-native";
+import { Button, Text, TextInput, View, Alert } from "react-native";
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignUpScreen({ setToken }) {
+  const [email, setEmail] = useState("yy@gmail.com");
+  const [username, setUsername] = useState("yiyi");
+  const [description, setDescription] = useState("I am Yiyi");
+  const [password, setPassword] = useState("reza");
+  const [confirmPassword, setConfirmPassword] = useState("reza");
+
+  const handlePress = async (event) => {
+    // const userToken = "secret-token";
+    //   setToken(userToken);
+    event.preventDefault();
+    // console.log(email, username, description, password, confirmPassword);
+    if (password == confirmPassword) {
+      try {
+        const response = await axios.post(
+          "https://express-airbnb-api.herokuapp.com/user/sign_up",
+          {
+            email: email,
+            username: username,
+            description: description,
+            password: password,
+            confirmPassword: confirmPassword,
+          }
+        );
+        // console.log(response.data);
+        if (response.data.token) {
+          setToken(response.data.token);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    } else {
+      Alert.alert("Passwords must be the same");
+    }
+  };
+
   return (
     <View>
       <View>
-        <Text>Name: </Text>
-        <TextInput placeholder="Username" />
-        <Text>Password: </Text>
-        <TextInput placeholder="Password" secureTextEntry={true} />
-        <Button
-          title="Sign up"
-          onPress={async () => {
-            const userToken = "secret-token";
-            setToken(userToken);
+        <Text>Email: </Text>
+        <TextInput
+          placeholder="email"
+          onChangeText={(text) => {
+            setEmail(text);
           }}
+          value={email}
         />
+        <Text>UserName: </Text>
+        <TextInput
+          placeholder="Username"
+          onChangeText={(text) => {
+            setUsername(text);
+          }}
+          value={username}
+        />
+        <Text>Description: </Text>
+        <TextInput
+          placeholder="Describe yourself in a few words"
+          onChangeText={(text) => {
+            setDescription(text);
+          }}
+          value={description}
+        />
+        <Text>Password: </Text>
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(text) => {
+            setPassword(text);
+          }}
+          value={password}
+        />
+        <Text>ConfirmPassword: </Text>
+        <TextInput
+          placeholder="confirm password"
+          secureTextEntry={true}
+          onChangeText={(text) => {
+            setConfirmPassword(text);
+          }}
+          value={confirmPassword}
+        />
+        <Button title="Sign up" onPress={handlePress} />
+        <Text> Already have an account? Sign in</Text>
       </View>
     </View>
   );
